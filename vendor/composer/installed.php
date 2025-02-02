aae9c55bda4cf67bb346360dce6e7a9df6e11acc
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-02-03 03:40:17 +0900

    依存関係をインストール

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/package',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f3fc7ade338b77cd3dc785e5570733a7c117f023',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/package' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f3fc7ade338b77cd3dc785e5570733a7c117f023',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
